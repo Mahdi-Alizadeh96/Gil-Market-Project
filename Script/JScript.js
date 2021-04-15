7b15eb7646e3e0e3049057904c78a025fc15b739
Mahdi A <mahdi.alizadeh.sp@gmail.com>
2021-04-16 00:41:07 +0430

    uploaded

---
+++ Script/JScript.js
@@ -0,0 +1,8 @@
+// dropdown menu
+$(document).ready(function(){
+    $('.dropdown-toggle').dropdown()
+});
+// tooltip
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
