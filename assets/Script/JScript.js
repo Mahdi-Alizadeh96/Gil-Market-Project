e257d896996f38ccea86dbe3c22cf37adf99d4a1
Mahdi A <mahdi.alizadeh.sp@gmail.com>
2021-05-20 20:37:49 +0430

    min-height for tow page

--- assets/Script/JScript.js
+++ assets/Script/JScript.js
@@ -6,3 +6,19 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+console.log('fuck you');
+
+  Callevents();
+
+  function Callevents() {
+      const getClass = document.querySelector('.alert alert-warning');
+      const addHeight = document.querySelector('shop-items');
+      console.log(addHeight);
+
+      if (getClass != null) {
+          getClass.style.display = 'flex';
+          addHeight.style.minHeight = '400px'
+          getClass.style.alignItems = 'center';
+      }
+  }
--- static_cdn/static_root/Script/JScript.js
+++ static_cdn/static_root/Script/JScript.js
@@ -6,3 +6,17 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
   });
+
+
+  Callevents();
+
+  function Callevents() {
+      const getClass = document.querySelector('.alert-warning');
+      const addHeight = document.querySelector('.shop-items');
+
+      if (getClass != null) {
+          getClass.style.display = 'flex';
+          addHeight.style.minHeight = '400px'
+          getClass.style.alignItems = 'center';
+      }
+  }
