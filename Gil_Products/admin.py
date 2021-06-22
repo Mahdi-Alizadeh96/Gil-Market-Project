ccc413166f1857ab22e91b2f0c6b011d068143c1
alishakuri <ali95shakur@gmail.com>
2021-06-22 16:18:39 +0430

    edit

--- Gil_Products/admin.py
+++ Gil_Products/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product, Gallery
-
+admin.site.site_header = "پنل مدیریت سایت"
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["__str__", 'name_fa', 'year', 'price', 'discount', 'active']
